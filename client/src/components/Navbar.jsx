552c09002722f16624c98c6245537960af0d5459
Edwyn Jones <edwyn__4@hotmail.com>
2025-11-04 21:03:10 +0100

    imported assets & Link from the dom and wrapped my image tag in the link and set it's directory.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import assets from '../assets'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-        
+        <Link to='/'>
+            <img src={assets.logo} alt="" className='h-8' />
+        </Link>
     </div>
   )
 }
