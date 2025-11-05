714dfebb9c625a5e0bba4c05414ce79694afe173
Edwyn Jones <edwyn__4@hotmail.com>
2025-11-05 20:31:13 +0100

    setShowLogin props is created which is used in Navbar and passed it which will ensure nav is visible on all pages; also created isOwner path which is wrapper on the Navbar. this ensures when user is in their dashboard nav will be removed and for the other pages it's shown.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerpath = useLocation().pathname.startsWith('/owner');
+
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      {!isOwnerpath && <Navbar setShowLogin={setShowLogin}/>}
+    </>
   )
 }
 
