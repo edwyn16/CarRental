d847144292112eb3343e8fbba44a4d2047e6abc4
Edwyn Jones <edwyn__4@hotmail.com>
2025-11-05 20:07:36 +0100

    added the onclick function to the buttons to re-direct the user to the correct view.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { use, useState } from 'react'
 import {assets, menuLinks} from '../assets/assets'
-import {Link, useLocation} from 'react-router-dom'
+import {Link, useLocation, useNavigate} from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const location = useLocation()
     const [open, setOpen] = useState(false)
+    const navigate = useNavigate()
 
   return (
     <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname === "/" && "bg-light"}`}>
@@ -28,8 +29,8 @@ const Navbar = () => {
             </div>
 
             <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
-                <button className='cursor-pointer'> Dashboard </button>
-                <button className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg'> Login </button>
+                <button onClick={() => navigate('/owner')} className='cursor-pointer'> Dashboard </button>
+                <button onClick={() => setShowLogin(true)} className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg'> Login </button>
             </div>
         </div>
     </div>
