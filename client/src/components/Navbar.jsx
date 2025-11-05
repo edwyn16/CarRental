1b810e9b2e0e5d87c3a8d18426821c43eafc00ce
Edwyn Jones <edwyn__4@hotmail.com>
2025-11-05 18:03:38 +0100

    created my links which are mapped and styled them; also added location path function which will display the bg-color based on the path.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import assets from '../assets'
-import {Link} from 'react-router-dom'
+import {assets, menuLinks} from '../assets'
+import {Link, useLocation} from 'react-router-dom'
 
 const Navbar = () => {
+
+    const location = useLocation();
+
   return (
     <div>
         <Link to='/'>
             <img src={assets.logo} alt="" className='h-8' />
         </Link>
+        <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"}`}>
+            {menuLinks.map((link, index) => (
+                <Link key={index} to={link.path}>
+                    {link.name}
+                </Link>
+            )) }
+        </div>
     </div>
   )
 }
