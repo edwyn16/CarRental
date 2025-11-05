e6db7fa5cef7f0aa93b66861ed6b797165281762
Edwyn Jones <edwyn__4@hotmail.com>
2025-11-05 19:34:53 +0100

    added search bar icon and it's styling.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,8 +22,9 @@ const Navbar = () => {
                 </Link>
             )) }
 
-            <div>
+            <div className='hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56'>
                 <input type="text" className='py-1.5 w-full bg-transparent outline-none placeholder-gray-500' placeholder='Search Cars' /> 
+                <img src={assets.search_icon} alt="search" />
             </div>
         </div>
     </div>
