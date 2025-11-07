630921d264396883cf9fd8ac4d3318a72ec801f7
Edwyn Jones <edwyn__4@hotmail.com>
2025-11-07 17:33:41 +0100

    updated css property.

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
         <h1 className='text-4xl md:text-5xl font-semibold'>Luxury cars on Rent</h1>
         <form className='flex flex-col md:flex-row items-start md:items-start justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
-            <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
+            <div className='flex flex-col md:flex-row items-start md:items-center gap-10 md:ml-8'>
                 <div>
                     <select required value={pickupLocation} onChange={(e) => setPickupLocation(e.target.value)}>
                         <option value="">Pickup Location</option>
