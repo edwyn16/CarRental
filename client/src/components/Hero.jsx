11e59a68bd7b2a0f4fe6568fe1e7ba303ab22314
Edwyn Jones <edwyn__4@hotmail.com>
2025-11-07 17:42:57 +0100

    added the pickup & return date inputs and also created the search btn.

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -16,7 +16,19 @@ const Hero = () => {
                     </select>
                     <p className='px-1 text-sm text-gray-500'> {pickupLocation ? pickupLocation : 'Please select location'}</p>
                 </div>
+                <div className='flex flex-col items-start gap-2'>
+                    <label htmlFor="pickup-date">Pickup Date</label>
+                    <input type="date" id="pickup-date" min={new Date().toISOString().split('T')[0]} className='text-sm text-gray-500' required/>
+                </div>
+                <div className='flex flex-col items-start gap-2'>
+                    <label htmlFor="return-date">Return Date</label>
+                    <input type="date" id="return-date" min={new Date().toISOString().split('T')[0]} className='text-sm text-gray-500' required/>
+                </div>
             </div>
+            <button className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer'>
+                <img src={assets.search_icon} alt="search" className='brightness-300' />
+                Search
+            </button>
         </form>
         <img src={assets.main_car} alt="car" className='max-h-74' />
     </div>
