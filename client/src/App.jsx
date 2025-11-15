b7c8f180cdae0e32c92f07fc7ca0836966f63af8
Edwyn Jones <edwyn__4@hotmail.com>
2025-11-15 17:57:01 +0100

    feat(routes): add owner dashboard routes (Layout, Dashboard, AddCar, ManageCars, ManageBookings)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import CarDetails from './pages/CarDetails.jsx'
 import Cars from './pages/Cars.jsx';
 import MyBookings from './pages/MyBookings.jsx';
 import Footer from './components/Footer.jsx';
+import Layout from './pages/owner/Layout.jsx';
+import Dashboard from './pages/owner/Dashboard.jsx';
+import AddCar from './pages/owner/AddCar.jsx';
+import ManageCars from './pages/owner/ManageCars.jsx';
+import ManageBookings from './pages/owner/ManageBookings.jsx';
 
 
 const App = () => {
@@ -21,6 +26,14 @@ const App = () => {
         <Route path='/car-details/:id' element={<CarDetails />} />
         <Route path='/cars' element={<Cars />} />
         <Route path='/my-bookings' element={<MyBookings />} />
+
+        {/* Owner Routes */}
+        <Route path='/owner' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='add-car' element={<AddCar />} />
+          <Route path='manage-cars' element={<ManageCars />} />
+          <Route path='manage-bookings' element={<ManageBookings />} />
+        </Route>
       </Routes>
 
       {!isOwnerpath && <Footer />}
